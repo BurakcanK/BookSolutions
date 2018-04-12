17b44ae2eb3289feb49ee1686ac71d83370db25a
Burak Can Kahraman <bck.coding@gmail.com>
2018-04-12 22:28:56 +0300

    Chapter01 solutions added.

---
+++ AutomateTheBoringStuffWithPython/Chapter03/validation.py
@@ -0,0 +1,28 @@
+""" The Collatz Sequence with Input Validation
+
+The given number always converges to 1.
+"""
+
+def collatz(num):
+    # if the num is even, divide it by two
+    if num % 2 == 0:
+        print(num // 2)
+        return num // 2
+    # else multiply it with 3 and add 1
+    else:
+        print(3 * num + 1)
+        return 3 * num + 1
+
+try:
+    # prompt the user for an integer
+    num = int(input("Enter number: "))
+
+    # continue calling collatz until result is 1
+    while True:
+        num = collatz(num)
+        if num == 1:
+            break
+
+# in case of invalid input
+except ValueError:
+    print("Input must be an integer.")
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BookSolutions
-Includes my personal solutions for the problems that are present in the books
+
+This repo represents my own solutions for the example or practice questions that are being asked in different coding books.
+
+It serves to my personal education and improvement.
+
+Alongside this if another fellow coder wants to give me tips to improve my coding I will be grateful.
