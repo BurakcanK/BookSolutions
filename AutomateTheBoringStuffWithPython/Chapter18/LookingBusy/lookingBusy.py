89be5e00a4f35ded6675cdc471ac9406a4073791
Burak Can Kahraman <bck.coding@gmail.com>
2018-06-21 21:39:44 +0300

    Chapter18 question 1 solved.

---
+++ AutomateTheBoringStuffWithPython/Chapter18/LookingBusy/lookingBusy.py
@@ -0,0 +1,15 @@
+""" Looking Busy
+
+Nudges the mouse cursor slightly every 10 seconds.
+"""
+
+import pyautogui
+import time
+
+while True:
+    # move the mouse a pixel and back
+    pyautogui.moveRel(0, 1)
+    pyautogui.moveRel(0, -1)
+
+    # wait
+    time.sleep(10)
