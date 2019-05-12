90d026a6cfe4d9c2b32f6faf257e8c14f39822ff
Burak Can Kahraman <bck.coding@gmail.com>
2019-05-12 15:43:26 +0300

    Alien invasion fleet completed.

--- PythonCrashCourse/AlienInvasio/alien_invasion.py
+++ PythonCrashCourse/AlienInvasio/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.sprite import Group
 
 import game_functions as gf
+from game_stats import GameStats
 from settings import Settings
 from ship import Ship
 
@@ -13,19 +14,28 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # make a ship
-    ship = Ship(ai_settings, screen)
+    # create an instance to store game statistics
+    stats = GameStats(ai_settings)
 
-    # make a group to store bullets in
+    # make a ship, a group of bullets and a group of aliens
+    ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+    # create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # start the main loop for the game
     while True:
         # watch for keyboard and mouse events
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
